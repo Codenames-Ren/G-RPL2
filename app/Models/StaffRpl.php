be466cc17a7be64efb7220f46aa5a6dc66b85431
Codenames-Ren <renmizuki38@gmail.com>
2026-05-23 13:33:28 +0700

    add model staff RPL

---
+++ app/Models/StaffRpl.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class StaffRpl extends Model
+{
+    protected $table = 'staff_rpl';
+
+    protected $fillable = [
+        'user_id',
+        'nip',
+        'phone',
+        'email_verified_at',
+        'is_active',
+    ];
+
+    /**
+     * User relation.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
