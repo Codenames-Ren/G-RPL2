098cd572a8860f6add5db24aef214fc13b6582db
Codenames-Ren <renmizuki38@gmail.com>
2026-06-01 22:28:19 +0700

    adjust Applicant model

--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Applicant extends Model
 {
@@ -11,16 +13,26 @@ class Applicant extends Model
         'nik',
         'phone',
         'address',
+        'birth_place',
+        'birth_date',
+        'gender',
+        'marital_status',
+        'nationality',
+        'postal_code',
+        'last_education',
+        'institution_name',
+        'study_program',
+        'graduation_year',
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(
             User::class
         );
     }
 
-    public function applications()
+    public function applications(): HasMany
     {
         return $this->hasMany(
             Application::class
