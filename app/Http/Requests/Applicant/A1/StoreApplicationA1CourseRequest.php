cc0c561fd240064ea114d1e2db053c043add4320
Codenames-Ren <renmizuki38@gmail.com>
2026-06-01 15:01:27 +0700

    create store application A1 request

---
+++ app/Http/Requests/Applicant/A1/StoreApplicationA1CourseRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests\Applicant\A1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreApplicationA1CourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Validation rules.
+     */
+    public function rules(): array
+    {
+        return [
+
+            'course_code' => [
+                'required',
+                'string',
+                'max:50',
+            ],
+
+            'course_name' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+
+            'credits' => [
+                'required',
+                'integer',
+                'min:1',
+                'max:10',
+            ],
+
+            'grade' => [
+                'required',
+                'string',
+                'max:5',
+            ],
+
+            'institution_name' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+        ];
+    }
+}
