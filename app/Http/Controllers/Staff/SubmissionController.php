d48724b08077a52f1729ee3f3299369604bee9aa
Codenames-Ren <renmizuki38@gmail.com>
2026-06-05 21:52:51 +0700

    add controller staff for submisson

--- app/Http/Controllers/Staff/SubmissionController.php
+++ app/Http/Controllers/Staff/SubmissionController.php
@@ -3,9 +3,120 @@
 namespace App\Http\Controllers\Staff;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+
+use App\Http\Requests\Staff\Submission\ReturnSubmissionRequest;
+use App\Http\Requests\Staff\Submission\AssignAssessorRequest;
+
+use App\Services\StaffSubmissionService;
 
 class SubmissionController extends Controller
 {
-    //
+    public function __construct(
+        protected StaffSubmissionService $staffSubmissionService
+    ) {}
+
+    /*
+    | Get All Submissions
+    */
+
+    public function index()
+    {
+        return response()->json([
+            'success' => true,
+
+            'data' => $this->staffSubmissionService
+                ->list(),
+        ]);
+    }
+
+    /*
+    | Get Submission Detail
+    */
+
+    public function show(
+        int $application
+    )
+    {
+        return response()->json([
+            'success' => true,
+
+            'data' => $this->staffSubmissionService
+                ->getById(
+                    $application
+                ),
+        ]);
+    }
+
+    /*
+    | Start Review
+    */
+
+    public function review(
+        int $application
+    )
+    {
+        return response()->json([
+
+            'success' => true,
+
+            'message'
+                => 'Application review started.',
+
+            'data'
+                => $this->staffSubmissionService
+                    ->review(
+                        $application
+                    ),
+        ]);
+    }
+
+    /*
+    | Return Application
+    */
+
+    public function return(
+        ReturnSubmissionRequest $request,
+        int $application
+    )
+    {
+        return response()->json([
+
+            'success' => true,
+
+            'message'
+                => 'Application returned successfully.',
+
+            'data'
+                => $this->staffSubmissionService
+                    ->return(
+                        $application,
+                        $request->validated()
+                    ),
+        ]);
+    }
+
+    /*
+    | Assign Assessor
+    */
+
+    public function assignAssessor(
+        AssignAssessorRequest $request,
+        int $application
+    )
+    {
+        return response()->json([
+
+            'success' => true,
+
+            'message'
+                => 'Assessor assigned successfully.',
+
+            'data'
+                => $this->staffSubmissionService
+                    ->assignAssessor(
+                        $application,
+                        $request->validated()
+                    ),
+        ]);
+    }
 }
