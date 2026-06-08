47e301240ae829d058052d86942ef6c3025c7d3e
Codenames-Ren <renmizuki38@gmail.com>
2026-06-08 14:15:20 +0700

    add store assessment request layer

---
+++ app/Http/Requests/Assessor/Assessment/StoreAssessmentRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests\Assessor\Assessment;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAssessmentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+
+            'notes' => [
+                'nullable',
+                'string',
+            ],
+        ];
+    }
+}
