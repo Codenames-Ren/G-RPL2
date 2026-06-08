c20386f1c950adbb495e3855cbf5e72b06f28191
Codenames-Ren <renmizuki38@gmail.com>
2026-06-08 19:54:02 +0700

    create committee controller

---
+++ app/Http/Controllers/Committee/CommitteeController.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Http\Controllers\Committee;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Committee\Approval\ApproveApplicationRequest;
+use App\Services\CommitteeService;
+use Illuminate\Http\Request;
+use App\Models\Application;
+use App\Enums\ApplicationStatus;
+
+class CommitteeController extends Controller
+{
+    public function __construct(
+        protected CommitteeService $committeeService
+    ) {}
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get Assessed Applications
+    |--------------------------------------------------------------------------
+    */
+
+    public function index()
+    {
+        return response()->json([
+
+            'success' => true,
+
+            'data'
+                => $this->committeeService
+                    ->listAssessedApplications(),
+        ]);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Get Application Detail
+    |--------------------------------------------------------------------------
+    */
+
+    public function show(
+        int $application
+    )
+    {
+        return response()->json([
+
+            'success' => true,
+
+            'data'
+                => $this->committeeService
+                    ->getById(
+                        $application
+                    ),
+        ]);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Preview Rector Decree
+    |--------------------------------------------------------------------------
+    */
+
+    public function previewRectorDecree(
+        int $application
+    )
+    {
+        return $this->committeeService
+            ->previewRectorDecree(
+                $application
+            );
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Download Rector Decree
+    |--------------------------------------------------------------------------
+    */
+
+    public function downloadRectorDecree(
+        int $application
+    )
+    {
+        return $this->committeeService
+            ->downloadRectorDecree(
+                $application
+            );
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Preview Assessment Summary
+    |--------------------------------------------------------------------------
+    */
+
+    public function previewAssessmentSummary(
+        int $application
+    )
+    {
+        return $this->committeeService
+            ->previewAssessmentSummary(
+                $application
+            );
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Download Assessment Summary
+    |--------------------------------------------------------------------------
+    */
+
+    public function downloadAssessmentSummary(
+        int $application
+    )
+    {
+        return $this->committeeService
+            ->downloadAssessmentSummary(
+                $application
+            );
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Approve Application
+    |--------------------------------------------------------------------------
+    */
+
+    public function approve(
+        ApproveApplicationRequest $request,
+        int $application
+    )
+    {
+        return response()->json([
+
+            'success' => true,
+
+            'message'
+                => 'Application approved successfully.',
+
+            'data'
+                => $this->committeeService
+                    ->approveApplication(
+                        $application,
+                        $request->validated()
+                    ),
+        ]);
+    }
+
+    public function approved()
+    {
+        return response()->json([
+
+            'success' => true,
+
+            'data' =>
+                $this->committeeService
+                    ->listApprovedApplications(),
+        ]);
+    }
+
+    public function showApproved(Application $application)
+    {
+        if ($application->status !== ApplicationStatus::APPROVED) {
+            abort(422, 'Application is not approved.');
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $this->committeeService->getApprovedApplicationDetail($application)
+        ]);
+    }
+}
