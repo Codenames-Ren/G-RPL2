d9124c7ee28d0a6a2be2ccdbe595772de18fed9e
Codenames-Ren <renmizuki38@gmail.com>
2026-06-08 19:57:12 +0700

    adjust email notifications

--- app/Notifications/VerifyEmailNotification.php
+++ app/Notifications/VerifyEmailNotification.php
@@ -2,13 +2,27 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-
-use Illuminate\Contracts\Queue\ShouldQueue;
-
 use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class VerifyEmailNotification extends VerifyEmail implements ShouldQueue
 {
     use Queueable;
+
+    public function toMail($notifiable): MailMessage
+    {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
+        return (new MailMessage)
+            ->subject('Verifikasi Email Akun RPL - ITB Bina Sarana Global')
+            ->view(
+                'emails.verify-email',
+                [
+                    'notifiable'       => $notifiable,
+                    'verificationUrl'  => $verificationUrl,
+                ]
+            );
+    }
 }
