8fa38b78998e6a02c62cacc38c77e2ce087edf73
Codenames-Ren <renmizuki38@gmail.com>
2026-07-03 21:21:08 +0700

    add migration table for grading

---
+++ database/migrations/2026_07_03_111049_add_grade_to_assessment_course_mappings_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('assessment_course_mappings', function (
+            Blueprint $table
+        ) {
+
+            $table->enum(
+                'grade',
+                [
+                    'A',
+                    'B',
+                    'C',
+                    'D',
+                    'E',
+                ]
+            )
+                ->nullable()
+                ->after('is_recognized');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('assessment_course_mappings', function (
+            Blueprint $table
+        ) {
+
+            $table->dropColumn('grade');
+        });
+    }
+};
